52bdaf853d653cf87d882ed99d7c8ad32b201737
nick-nugat <onestoptube@gmail.com>
2024-02-23 23:36:51 -0500

    moved project design documents to coding-notes repo

---
+++ projects/NguyenNicholas_Assignment3/src/SortedDoubleLinkedList.java
@@ -0,0 +1,41 @@
+/**
+ * @author Nicholas Nguyen
+ */
+
+import java.util.Comparator;
+import java.util.ListIterator;
+
+public class SortedDoubleLinkedList<T> extends BasicDoubleLinkedList<T>{
+	private Comparator<T> compareableObject;
+
+	public SortedDoubleLinkedList(Comparator<T> compareableObject){
+		//todo implement constructor
+	}
+
+	public void add(T data){
+		//todo implement add method (other add methods don't work and will throw exceptions)
+	}
+
+
+	@Override
+	public ListIterator<T> iterator(){
+		return super.iterator();
+	}
+
+	@Override
+	public Node remove(T data, Comparator<T> comparator){
+		return super.remove(data, comparator);
+	}
+
+	@Override
+	public void addToEnd(T data) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException("Invalid operation for sorted list.");
+	}
+
+	@Override
+	public void addToFront(T data) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException("Invalid operation for sorted list.");
+	}
+
+
+}
