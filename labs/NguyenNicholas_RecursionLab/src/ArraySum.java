165f7a3b503b1b0b62748c2a7081bce435e83ebb
nick-nugat <onestoptube@gmail.com>
2024-02-28 13:30:35 -0500

    recursion lab finished

---
+++ labs/NguyenNicholas_RecursionLab/src/ArraySum.java
@@ -0,0 +1,11 @@
+/**
+ * @author Nicholas Nguyen
+ */
+
+public class ArraySum {
+	public int sumOfArray (Integer[] a, int index){
+		if (index < 0)
+			return 0;
+		return (sumOfArray(a, index - 1) + a[index]);
+	}
+}
