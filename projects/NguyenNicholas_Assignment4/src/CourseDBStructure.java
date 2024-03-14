ea1ae8cb97c896a28cd7287c02dc6def7de0a0d6
nick-nugat <onestoptube@gmail.com>
2024-03-14 14:50:43 -0400

    possible implementation for add() method - added comments to possibly help with continuing

--- projects/NguyenNicholas_Assignment4/src/CourseDBStructure.java
+++ projects/NguyenNicholas_Assignment4/src/CourseDBStructure.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * @author Nicholas Nguyen
  */
 
+//implement a hash table with buckets
 public class CourseDBStructure implements CourseDBStructureInterface {
+	private static final double LOAD_FACTOR = 1.5;
+	//each element will have a hash code calculated based on CRN (since it's unique)
+	//however, tests require hashcode of String
+	private LinkedList<CourseDBElement>[] hashTable;
+	private int tableLength;
+
 	/*This class has two constructors:
 			1)	A constructor that takes in an integer n which represents the estimated number of
 	courses and determines the size of the hash table by finding a 4K+3 prime just greater than n /loading factor.
@@ -13,7 +21,13 @@ public class CourseDBStructure implements CourseDBStructureInterface {
 			2)	A Constructor for testing purposes. This constructor will take a string “Testing” and an int for the hashtable size.  This is used only for testing.
 */
 
-	public CourseDBStructure(int n){}
+	/**
+	 * Constructor to determine table length
+	 * @param n estimated number of courses
+	 */
+	public CourseDBStructure(int n){
+
+	}
 
 	//constructor for testing purposes (?)
 	public CourseDBStructure(String testing, int hashTableSize){}
@@ -22,7 +36,15 @@ public class CourseDBStructure implements CourseDBStructureInterface {
 
 	@Override
 	public void add(CourseDBElement element) {
+		int index = element.getCrn() % tableLength;
+		LinkedList<CourseDBElement> bucket = hashTable[index];
 
+		for (CourseDBElement e : bucket) {
+			if (element.getCrn() == e.getCrn()){
+				return;
+			}
+		}
+		bucket.add(element);
 	}
 
 	@Override
