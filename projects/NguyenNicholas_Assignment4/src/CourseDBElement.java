c14ff928af263aff9bca3a0c7939ac24316aa498
nick-nugat <onestoptube@gmail.com>
2024-03-14 14:51:23 -0400

    added getters and setters i might not need the setters though

--- projects/NguyenNicholas_Assignment4/src/CourseDBElement.java
+++ projects/NguyenNicholas_Assignment4/src/CourseDBElement.java
@@ -13,4 +13,44 @@ public class CourseDBElement implements Comparable {
 	public int compareTo(Object o) {
 		return 0;
 	}
+
+	public String getCourseID() {
+		return courseID;
+	}
+
+	public void setCourseID(String courseID) {
+		this.courseID = courseID;
+	}
+
+	public int getCrn() {
+		return crn;
+	}
+
+	public void setCrn(int crn) {
+		this.crn = crn;
+	}
+
+	public int getCredits() {
+		return credits;
+	}
+
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public String getInstructorName() {
+		return instructorName;
+	}
+
+	public void setInstructorName(String instructorName) {
+		this.instructorName = instructorName;
+	}
 }
