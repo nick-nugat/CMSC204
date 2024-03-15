4e46d1808b78e136fdd19cee65b43cde8155d5b0
nick-nugat <onestoptube@gmail.com>
2024-03-15 17:09:37 -0400

    tryna make progress on manager lol

--- projects/NguyenNicholas_Assignment4/src/CourseDBManager.java
+++ projects/NguyenNicholas_Assignment4/src/CourseDBManager.java
@@ -7,22 +7,47 @@ import java.util.ArrayList;
  */
 
 public class CourseDBManager implements CourseDBManagerInterface {
+	CourseDBStructure structure;
 
+
+	/**
+	 *
+	 * @param id course id
+	 * @param crn course crn
+	 * @param credits number of credits
+	 * @param roomNum course room number
+	 * @param instructor name of the instructor
+	 */
 	@Override
 	public void add(String id, int crn, int credits, String roomNum, String instructor) {
-
+		CourseDBElement element = new CourseDBElement(id, crn, credits, roomNum, instructor);
+		structure.add(element);
 	}
 
+	/**
+	 *
+	 * @param crn course crn (key)
+	 * @return
+	 */
 	@Override
 	public CourseDBElement get(int crn) {
 		return null;
 	}
 
+	/**
+	 *
+	 * @param input input file
+	 * @throws FileNotFoundException
+	 */
 	@Override
 	public void readFile(File input) throws FileNotFoundException {
 
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public ArrayList<String> showAll() {
 		return null;
