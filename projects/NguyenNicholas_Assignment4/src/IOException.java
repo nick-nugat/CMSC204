3e94c21c766f4e7dc2520f847f6a54a95cc972fb
nick-nugat <onestoptube@gmail.com>
2024-03-15 22:13:20 -0400

    prob dont need to create a separate ioexception file

--- projects/NguyenNicholas_Assignment4/src/IOException.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Nicholas Nguyen
- */
-
-//not sure if i needed to create this tbh
-
-//thrown when user selects input file that cant be read, attempting to retrieve a CDE that does not exist int he DB
-public class IOException extends Exception {
-}
